cf0a1dc4be47c4c6c369c10762660689b24356fa
highsunmom <highsunmom@highsunmom.com>
2026-03-14 23:36:35 +0900

    경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 반드시 레포지토리 이름과 일치해야 해! 앞뒤 슬래시(/) 필수.
+  base: '/HighSunNews/', 
 })
